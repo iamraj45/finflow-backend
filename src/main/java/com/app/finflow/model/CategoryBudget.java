9aab5d0fd755e73ee35316ca1259bab964edbc1b
rahulkumar <rahulkumar980850@gmail.com>
2025-04-18 14:37:08 +0530

    get category budget - 2

--- src/main/java/com/app/finflow/model/CategoryBudget.java
+++ src/main/java/com/app/finflow/model/CategoryBudget.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Entity
 @Data
-@Table(name = "catergory_budget")
+@Table(name = "category_budget")
 public class CategoryBudget {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
